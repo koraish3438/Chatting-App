21ec007fdcbeb32e7368590f79f782fe5d1f2399
koraish3438 <koraishmdkajamall38@gmail.com>
2025-11-17 10:27:54 +0600

    2nd commit

--- app/src/main/java/com/example/chatingapp/ui/ChatScreenActivity.kt
+++ app/src/main/java/com/example/chatingapp/ui/ChatScreenActivity.kt
@@ -3,8 +3,6 @@ package com.example.chatingapp.ui
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.chatingapp.R
 
 class ChatScreenActivity : AppCompatActivity() {
@@ -12,10 +10,6 @@ class ChatScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_chat_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
--- app/src/main/java/com/example/chatingapp/ui/SignUpActivity.kt
+++ app/src/main/java/com/example/chatingapp/ui/SignUpActivity.kt
@@ -3,8 +3,6 @@ package com.example.chatingapp.ui
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.chatingapp.R
 
 class SignUpActivity : AppCompatActivity() {
@@ -12,10 +10,6 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_sign_up)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
